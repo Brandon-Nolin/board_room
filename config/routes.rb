0a0b064b31ce1d62f8f3b72d8e3774d1f6a158c6
Brandon Nolin <brandonolinn@gmail.com>
2024-03-26 09:35:46 -0500

    Adds tailwind and creates Games index page
    Setup tailwind in the project and added the index page for Games.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :games, only: [:index, :show]
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
