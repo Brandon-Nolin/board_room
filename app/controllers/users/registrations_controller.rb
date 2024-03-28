72cf7a8b2599de56a31386d50ad47def83e335de
Brandon Nolin <brandonolinn@gmail.com>
2024-03-28 11:33:27 -0500

    Creates user edit page
    Users can now edit their account information and change their password.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_update_params, only: [:update]
+
   private
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :city, :address, :postal_code, :province_id)
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    # Require current password if user is trying to change password.
+    return super if params["password"].present?
+
+    # Allows user to update registration information without password.
+    resource.update_without_password(params.except("current_password"))
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :city, :address, :postal_code, :province_id])
   end
 end
