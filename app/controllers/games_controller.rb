beb56b094984e4c6a5b6719a739cf916bb4307be
Brandon Nolin <brandonolinn@gmail.com>
2024-04-01 22:06:18 -0500

    Modifies games page to allow viewing sales/recently added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,12 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    if params.key?(:on_sale)
+      @games = Game.where.not(sale_price: nil)
+    elsif params.key?(:recently_added)
+      @games = Game.where("created_at >= ?", 3.days.ago)
+    else
+      @games = Game.all
+    end
   end
 
   def show
