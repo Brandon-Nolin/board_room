488a712d3ad1942a939e3b4373730c9779bf84be
Brandon Nolin <brandonolinn@gmail.com>
2024-04-02 15:37:23 -0500

    Adds pagination through kaminari to /games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,11 @@
 class GamesController < ApplicationController
   def index
     if params.key?(:on_sale)
-      @games = Game.where.not(sale_price: nil)
+      @games = Game.where.not(sale_price: nil).page(params[:page]).per(20)
     elsif params.key?(:recently_added)
-      @games = Game.where("created_at >= ?", 3.days.ago)
+      @games = Game.where("created_at >= ?", 3.days.ago).page(params[:page]).per(20)
     else
-      @games = Game.all
+      @games = Game.all.page(params[:page]).per(20)
     end
   end
 
