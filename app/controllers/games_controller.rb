9a0fd537530a86ad87d84dd1c3b72069c453bf47
Brandon Nolin <brandonolinn@gmail.com>
2024-04-07 10:38:30 -0500

    Adds functionality to search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,4 +12,26 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def search
+    @search_term = "%#{params[:keywords]}%"
+
+    if params[:category] && params[:category][:name]
+      @category = params[:category][:name]
+    else
+      @category = ""
+    end
+    puts @category
+
+    if @category.present?
+      @games = Game.joins(:categories)
+                .where("games.name LIKE ? AND categories.name = ?", @search_term, @category)
+                .page(params[:page]).per(20)
+    else
+      @games = Game.where("name LIKE ?", @search_term)
+                .page(params[:page]).per(20)
+    end
+
+    render :index
+  end
 end
